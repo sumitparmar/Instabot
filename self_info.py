9e4b3827020efcdea134b26302c97c476e20c7f8
sumitparmar <sumitparmar496@gmailcom>
2017-07-06 00:00:18 +0530

    new file self_info is created

---
+++ self_info.py
@@ -0,0 +1,7 @@
+#here we define a function named as "self_info"
+from main import APP_ACCESS_TOKEN
+from main import BASE_URL
+def self_info():
+    request_url = (BASE_URL + 'users/self/?access_token=%s') % (APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
